99c68809554f87ab91e3e428b0b746d1d5362707
tayef <tayefhossain200@gmail.com>
2025-11-29 23:19:52 +0600

    add FPS

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
 
-
-
-
-
 def main():
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -20,8 +17,9 @@ def main():
                 return
         
         screen.fill("black")
-        pygame.display.flip()
-
+        pygame.display.flip() 
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
