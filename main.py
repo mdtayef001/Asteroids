5a9891629d20da179c30c11e3ea5724f28833d6a
tayef <tayefhossain200@gmail.com>
2025-12-09 18:48:23 +0600

    add the player roate method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -8,6 +9,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -16,8 +18,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip() 
+
         dt = clock.tick(60) / 1000
         
 
---
+++ player.py
@@ -0,0 +1,32 @@
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+import pygame
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+    
+
+    def draw(self, screen):
+        pygame.draw.polygon(surface=screen, color="white", points=self.triangle(), width=LINE_WIDTH)
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+    
+
+    def rotate(self, dt):
+        self.rotation = PLAYER_TURN_SPEED *  dt
